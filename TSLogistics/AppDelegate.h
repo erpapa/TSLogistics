d23f00b51012b56ba18df8197b46fbb175a003a6
erpapa <hyplc@163.com>
2015-11-09 14:29:19 +0800

    Initial Commit

---
+++ TSLogistics/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TSLogistics
+//
+//  Created by erpapa on 15/11/9.
+//  Copyright (c) 2015年 erpapa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TSLogistics/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TSLogistics
+//
+//  Created by erpapa on 15/11/9.
+//  Copyright (c) 2015年 erpapa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
